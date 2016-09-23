352b06ba6e9849ba669d59694b0aa1404ed37812
rattri <bisnis.ratri@gmail.com>
2016-09-24 02:53:36 +0700

    edit layout login register

--- app/src/main/java/com/solfacell/MainActivity.java
+++ app/src/main/java/com/solfacell/MainActivity.java
@@ -1,5 +1,6 @@
 package com.solfacell;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+
 
     private int[] tabIcons = {
 
@@ -159,18 +161,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.beranda) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.login) {
+            Intent i = new Intent(this,  PriceListActivity.class);
+            startActivity(i);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
