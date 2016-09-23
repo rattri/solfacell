fe036689c64d4699b97e7965949ad15613a4eaaf
rattri <bisnis.ratri@gmail.com>
2016-09-24 01:44:15 +0700

    add constan class

---
+++ app/src/main/java/com/solfacell/constantclass/ConstanLoginRegister.java
@@ -0,0 +1,24 @@
+package com.solfacell.constantclass;
+
+/**
+ * Created by Ratri on 9/23/2016.
+ */
+public class ConstanLoginRegister {
+
+        public static final String BASE_URL = "http://solfacell.solfagaming.com/";
+        public static final String REGISTER_OPERATION = "register";
+        public static final String LOGIN_OPERATION = "login";
+        public static final String CHANGE_PASSWORD_OPERATION = "chgPass";
+
+        public static final String SUCCESS = "success";
+        public static final String FAILURE = "failure";
+        public static final String IS_LOGGED_IN = "isLoggedIn";
+
+        public static final String NAME = "name";
+        public static final String EMAIL = "email";
+        public static final String UNIQUE_ID = "unique_id";
+
+        public static final String TAG = "Learn2Crack";
+}
+
+
